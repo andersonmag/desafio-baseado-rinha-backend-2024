5d8d35f8acad25417da6002eec316393d415dc46
Anderson <andersondel.dev@gmail.com>
2025-03-17 20:05:28 -0300

    feature: add transaction class

---
+++ backend-java/src/main/java/com/github/andersonmag/backendjava/models/entities/Transacao.java
@@ -0,0 +1,91 @@
+package com.github.andersonmag.backendjava.models.entities;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.github.andersonmag.backendjava.models.enums.TipoTransacao;
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+
+@Entity(name = "transacoes")
+public class Transacao {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @JsonIgnore
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "cliente_id")
+    private Cliente cliente;
+    private Long valor;
+    @Enumerated(EnumType.STRING) // <- IMPORTANTE: Salva como texto ('r' ou 'd')
+    private TipoTransacao tipo;
+    private String descricao;
+    private LocalDateTime realizadaEm;
+
+    public Transacao(Long valor, TipoTransacao tipo, String descricao) {
+        this.valor = valor;
+        this.tipo = tipo;
+        this.descricao = descricao;
+        this.realizadaEm = LocalDateTime.now();
+    }
+
+    public Transacao(Long valor, TipoTransacao tipo, String descricao, Cliente cliente) {
+        this.valor = valor;
+        this.tipo = tipo;
+        this.descricao = descricao;
+        this.realizadaEm = LocalDateTime.now();
+        this.cliente = cliente;
+    }
+
+    public Transacao() {
+    }
+
+    public Cliente getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(Cliente cliente) {
+        this.cliente = cliente;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getValor() {
+        return valor;
+    }
+
+    public void setValor(Long valor) {
+        this.valor = valor;
+    }
+
+    public TipoTransacao getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(TipoTransacao tipo) {
+        this.tipo = tipo;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public LocalDateTime getRealizadaEm() {
+        return realizadaEm;
+    }
+
+    public void setRealizadaEm(LocalDateTime realizadaEm) {
+        this.realizadaEm = realizadaEm;
+    }
+}
