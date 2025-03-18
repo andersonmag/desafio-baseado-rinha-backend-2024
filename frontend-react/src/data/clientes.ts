bcfd8749425552a422dcb89214d8cb13804fbff6
Anderson <andersondel.dev@gmail.com>
2025-03-18 14:52:46 -0300

    feature: add customers mock

---
+++ frontend-react/src/data/clientes.ts
@@ -0,0 +1,14 @@
+export interface Cliente {
+    id: number;
+    nome: string;
+    limite: number;
+}
+
+export const clientes: Cliente[] = [
+    { id: 1, nome: "o barato sai caro", limite: 1000 * 100},
+    { id: 2, nome: "zan corp ltda", limite: 800 * 100},
+    { id: 3, nome: "les cruders", limite: 10000 * 100},
+    { id: 4, nome: "padaria joia de cocaia", limite: 100000 * 100},
+    { id: 5, nome: "kid mais", limite: 5000 * 100},
+    { id: 6, nome: "cliente com id não cadastrado", limite: 0},
+];
